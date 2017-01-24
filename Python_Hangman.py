a0b69df288e0bad2898eb8a62ee33e2040f7e6f6
Gerrit Ohrner <ohrner90@googlemail.com>
2017-01-24 19:14:19 +0100

    Revert "Almost done"
    This reverts commit 3bba16571dbe8ee82885002b3e5561ba6e04d285.

--- Python_Hangman.py
+++ Python_Hangman.py
@@ -13,9 +13,8 @@ os.system('cls' if os.name == 'nt' else 'clear')
 i = 1
 word = ''
 wordCheck = []
-wordBlank = []
+wordBlank = ''
 difficulty = 0
-tries = 0
 def wortEingabe():
 	global i
 	global word
@@ -27,7 +26,7 @@ def wortEingabe():
 def schwierigkeit():
 	global difficulty
 	print("""
-	Please choose a difficulty level:
+	Please chosse a difficulty level:
 	1. Easy
 	2. Medium
 	3. Hard
@@ -39,7 +38,7 @@ def guessing():
 	global wordLength
 	wordGuess = input("Bitte gib einen Buchstaben ein (a-z): ")
 	wordLength = len(wordGuess)
-	#Schleife zum checken, ob die Eingabe ein Zahl beinhaltet/länger als 1 Zeichen ist.
+	#Schleife zum checknen, ob die Eingabe ein Zahl beinhaltet/länger als 1 Zeichen ist.
 	while len(wordGuess) > 1 or wordGuess.isdigit() == True:
 		if len(wordGuess) > 1:
 			wordGuess = input('Bitte maximal einen Buchstaben eingeben: ')
@@ -47,41 +46,18 @@ def guessing():
 		elif wordGuess.isdigit() == True:
 			wordGuess = input('Die eingabe sollte keine zahl sein: ')
 			continue
-#
-"""
-Funktion die überprüft, ob der eingegebene Buchstabe im zu suchenden Wort zu finden ist.
-def wordCheck():
-	global splitWord
-	global wordBlank
-	global tries
-	global wordGuess
 
-	while i < len(splitWord)
-		if splitWord[i] == wordGuess
-			wordBlank[i] = wordGuess
-		elif
-			stuff
-	
-if Buchstabe in wort:
-	geupdatetes wort zurückgeben
-elif Buchstabe NICHT in wort:
-	tires += 1
 
-"""
+
+
 schwierigkeit()
-#Schleife zum checken ob difficulty wirklich eine Zahl ist.
+# Schleife zum checken ob difficulty wirklich eine Zahl ist.
 while difficulty != '1' and difficulty != '2' and difficulty != '3':
 	os.system('cls' if os.name == 'nt' else 'clear')
 	print('Ungueltige Eingabe!')
 	schwierigkeit()
 
-if difficulty == '1':
-	difficulty = 10
-elif difficulty == '2':
-	difficulty = 7
-elif difficulty == '3':
-	difficulty = 4
-
+difficulty = int(difficulty)
 wortEingabe()
 # Schleife zum pruefen ob das Wort eine Zahl beinhaltet
 while i < len(word):
@@ -92,32 +68,35 @@ while i < len(word):
 	i += 1
 i = 0
 
-#Schleife um in der Variable "wordBlank" so viele "_" einzutragen, wie es Buchstaben in eingegebenen Wort gibt.
-while i < len(word):
-	wordBlank.append('_')
-	i += 1
+guessing()
+#HIer noch das word in wordbLank als "_" speichern
+for i in word[i]:
+	wordBlank[i] = '_'
 
-i = 0
-splitWord = list(word)
-joinWord = ''
-wordBlank = joinWord.join(wordBlank)
-
-#Hauptschleife
+#Schleife zum erraten des Wortes. Solange es noch _ enthält, schleife wiederholen.
 while wordBlank != word:
-	os.system('cls' if os.name == 'nt' else 'clear')
-	if tries = difficulty:
-		print('Du hast zu viele Versuche benötigt')
-		print('Game Over')
-		input()
-		break
-	print('Es bleiben noch ', difficulty - tries, ' Fehlschlaege')
-	guessing()
-	wordBlank = list(wordBlank)
-#	wordCheck()
-	wordBlank = joinWord.join(wordBlank)
-	print(word)
-	print(wordBlank)
+	
+	for i in 
 
 
-print('End of ULTRA HARDCORE Prog :D')
-input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
