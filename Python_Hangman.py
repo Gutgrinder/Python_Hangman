5deb975cd09ba9178e45c89e699241408e91ffeb
Gerrit Ohrner <ohrner90@googlemail.com>
2017-01-24 19:26:25 +0100

    wefsd
    dfvsfv

--- Python_Hangman.py
+++ Python_Hangman.py
@@ -9,12 +9,60 @@
 #Wort ausgeben mit dem "eventuell Korrekt geratenen" Buchstaben eingesetzt
 import os
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####
 os.system('cls' if os.name == 'nt' else 'clear')
 i = 1
 word = ''
 wordCheck = []
-wordBlank = ''
+wordBlank = []
 difficulty = 0
+tries = 0
+wordBackup = []
 def wortEingabe():
 	global i
 	global word
@@ -26,7 +74,7 @@ def wortEingabe():
 def schwierigkeit():
 	global difficulty
 	print("""
-	Please chosse a difficulty level:
+	Please choose a difficulty level:
 	1. Easy
 	2. Medium
 	3. Hard
@@ -38,7 +86,7 @@ def guessing():
 	global wordLength
 	wordGuess = input("Bitte gib einen Buchstaben ein (a-z): ")
 	wordLength = len(wordGuess)
-	#Schleife zum checknen, ob die Eingabe ein Zahl beinhaltet/länger als 1 Zeichen ist.
+#Funktion zum checken, ob die Eingabe ein Zahl beinhaltet/länger als 1 Zeichen ist.
 	while len(wordGuess) > 1 or wordGuess.isdigit() == True:
 		if len(wordGuess) > 1:
 			wordGuess = input('Bitte maximal einen Buchstaben eingeben: ')
@@ -46,18 +94,47 @@ def guessing():
 		elif wordGuess.isdigit() == True:
 			wordGuess = input('Die eingabe sollte keine zahl sein: ')
 			continue
+#
 
-
-
+#Funktion die überprüft, ob der eingegebene Buchstabe im zu suchenden Wort zu finden ist.
+def wordNew():
+	global splitWord
+	global wordBlank
+	global tries
+	global wordGuess
+	global wordBackup
+	global i
+	wordBackup = wordBlank
+	print(wordBackup)
+	print(wordBlank)
+	while i < len(splitWord):
+		if splitWord[i] == wordGuess:
+			wordBlank[i] = wordGuess
+		i += 1
+	print(wordBackup)
+	print(wordBlank)
+	if wordBlank == wordBackup:
+		tries += 1
+	i = 0
+	input()
+#
 
 schwierigkeit()
-# Schleife zum checken ob difficulty wirklich eine Zahl ist.
+#Schleife zum checken ob difficulty wirklich eine Zahl ist.
 while difficulty != '1' and difficulty != '2' and difficulty != '3':
 	os.system('cls' if os.name == 'nt' else 'clear')
 	print('Ungueltige Eingabe!')
 	schwierigkeit()
+#
 
-difficulty = int(difficulty)
+if difficulty == '1':
+	difficulty = 10
+elif difficulty == '2':
+	difficulty = 7
+elif difficulty == '3':
+	difficulty = 4
+#
+
 wortEingabe()
 # Schleife zum pruefen ob das Wort eine Zahl beinhaltet
 while i < len(word):
@@ -67,36 +144,33 @@ while i < len(word):
 		wortEingabe()
 	i += 1
 i = 0
+#
 
-guessing()
-#HIer noch das word in wordbLank als "_" speichern
-for i in word[i]:
-	wordBlank[i] = '_'
+#Schleife um in der Variable "wordBlank" so viele "_" einzutragen, wie es Buchstaben in eingegebenen Wort gibt.
+while i < len(word):
+	wordBlank.append('_')
+	i += 1
+i = 0
+splitWord = list(word)
+joinWord = ''
+wordBlank = joinWord.join(wordBlank)
+#
 
-#Schleife zum erraten des Wortes. Solange es noch _ enthält, schleife wiederholen.
+#Hauptschleife
 while wordBlank != word:
-	
-	for i in 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print(wordBlank)
+	if tries == difficulty:
+		print('Du hast zu viele Versuche benötigt')
+		print('Game Over')
+		input()
+		break
+	print('Es bleiben noch ', difficulty - tries, ' Fehlschlaege')
+	guessing()
+	wordBlank = list(wordBlank)
+	wordNew()
+	wordBlank = joinWord.join(wordBlank)
+os.system('cls' if os.name == 'nt' else 'clear')
+print('Glueckwunsch, richtig geraten. Das eingegebene Wort ist:', wordBlank)
+print('End of ULTRA HARDCORE Prog :D')
+input()
